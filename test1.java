2ba7bbda368cb4b6e0a87ee8cb475206a64c6407
MarshmallowBox <48012363+MarshmallowBox@users.noreply.github.com>
2019-05-16 16:37:46 +0900

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class test1 extends JFrame {
+		private JPanel panel;
+		private JTextField display;
+		private JButton[] buttons;
+		private String[] labels = { "Backspace", "", "", "CE", "C", "7", "8", "9",
+				"/", "sqrt", "4", "5", "6", "x", "%", "1", "2", "3", "-", "1/x",
+				"0", "-/+", ".", "+", "=", };
+		private double result = 0;
+		private String operator = "=";
+		private boolean startOfNumber = true;
+		
+			
+	public static void main(String[] args) {
+		test1 s = new test1();
+
+	}
+	public test1() {
+		display = new JTextField(35);
+		panel = new JPanel();
+		display.setText("0.0");
+		//display.setEnabled(true);
+		panel.setLayout(new GridLayout(0, 5, 3, 3));
+		buttons = new JButton[25];
+		int index = 0;
+		for (int rows = 0; rows < 5; rows++) {
+			for (int cols = 0; cols < 5; cols++) {
+				buttons[index] = new JButton(labels[index]);
+				if (cols >= 3)
+					buttons[index].setForeground(Color.red);
+				else
+					buttons[index].setForeground(Color.blue);
+				buttons[index].setBackground(Color.yellow);
+				panel.add(buttons[index]);
+				//buttons[index].addActionListener(this);
+				index++;
+			}
+		}
+		add(display, BorderLayout.NORTH);
+		add(panel, BorderLayout.CENTER);
+		setVisible(true);
+		pack();
+	}
+
+	public double getResult() {
+		return result;
+	}
+	public void setResult(double result) {
+		this.result = result;
+	}
+
+
+}
